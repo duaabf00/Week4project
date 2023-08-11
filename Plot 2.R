5486016a5e9e9b00f8fffb79f95abcf6356f1f8f
Duaa Fahs <132445850+duaabf00@users.noreply.github.com>
2023-08-11 23:11:34 +0300

    Add files via upload

---
+++ Plot 2.R
@@ -0,0 +1,10 @@
+ig <- readRDS("exp-data/summarySCC_PM25.rds")
+SCC <- readRDS("exp-data/Source_Classification_Code.rds")
+
+ig_sample <- ig[sample(nrow(ig), size = 5000, replace = F), ]
+
+# Subsetting data 
+ig_play <- subset(ig, fips == '24510')
+png(filename = 'exp-data/xxx-2/plot2.png')
+barplot(tapply(X = ig_play$Emissions, INDEX = ig_play$year, FUN = sum), main = 'Total Emission in Baltimore City, ig_play', xlab = 'Year', ylab = expression('PM'[2.5]))
+dev.off()
